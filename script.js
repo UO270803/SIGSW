6fca586b9f2375a67b1b10d4389c8ee5226bb649
davidaf3 <david.alvarez.fidalgo@gmail.com>
2023-10-18 19:52:59 +0200

    Mejoras en busqueda de rutas

--- script.js
+++ script.js
@@ -25,9 +25,7 @@ function initMap() {
     initialize(defaultCenter);
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position => {
-            const center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map.setCenter(center);
-            myPosition.setPosition(center);
+            setCenter(position.coords.latitude, position.coords.longitude);
         }));
     }
 }
@@ -41,7 +39,7 @@ function getPrice(price) {
     else return price.trim().substring(0, 4);
 }
 
-function loadGoogleAutocomplete(center) {
+function loadGoogleAutocomplete() {
     const inputOrigen = document.getElementById("inputOrigen");
     const inputDestino = document.getElementById("inputDestino");
 
@@ -60,23 +58,18 @@ function loadGoogleAutocomplete(center) {
 
     new google.maps.places.Autocomplete(inputOrigen, options);
     new google.maps.places.Autocomplete(inputDestino, options);
-
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({location: center}, (results, status) => {
-        if(status === google.maps.GeocoderStatus.OK && results[0])
-            inputOrigen.value = results[0].formatted_address;
-    });
 }
 
 function initialize(center) {
-    loadGoogleAutocomplete(center);
+    loadGoogleAutocomplete();
 
     directionsDisplay = new google.maps.DirectionsRenderer({
         polylineOptions: {
             strokeColor: '#3282ea', // Color de la línea de la ruta
             strokeOpacity: 1.0,
             strokeWeight: 7 // Grosor de la línea de la ruta (ajústalo según tus necesidades)
-        }
+        },
+        suppressMarkers: true
     });
     directionsService = new google.maps.DirectionsService();
 
@@ -94,6 +87,8 @@ function initialize(center) {
     });
     myPosition.setZIndex(1000);
 
+    setCenter(center);
+
     var overlayOptions =
     {
         getTileUrl: TileWMS,
@@ -102,7 +97,7 @@ function initialize(center) {
     var overlayWMS = new google.maps.ImageMapType(overlayOptions);
     map.overlayMapTypes.push(overlayWMS);
     directionsDisplay.setMap(map);
-    getCargadores();
+    getCargadores().then(cargadores => cargadores.forEach(addMarcadorCargador));
 }
 
 function calcularRuta(e) {
@@ -119,7 +114,8 @@ function calcularRuta(e) {
 
     directionsService.route(peticion, async function (response, status) {
         if (status !== google.maps.DirectionsStatus.OK) return;
-        limpiarRuta();
+        limpiarMarcadores();
+        directionsDisplay.setDirections({routes: []});
 
         let rutaRestante = response.routes[0].overview_path;
         let cargaRestante = cargaBateria;
@@ -155,13 +151,14 @@ function calcularRuta(e) {
 }
 
 async function getMejorCargador(ruta, autonomia, cargaBateria) {
+    const radioBusqueda = 15;
     const autonomiaRestante =  autonomia * cargaBateria;
     let dist = 0;
     let puntoParaBuscar = null;
 
     for (let i = 0; i < ruta.length - 1; i++) {
         dist += google.maps.geometry.spherical.computeDistanceBetween(ruta[i], ruta[i + 1]) / 1000;
-        if (dist + 50 > autonomiaRestante) {
+        if (dist + radioBusqueda * 2 > autonomiaRestante) {
             puntoParaBuscar = i + 1;
             break;
         }
@@ -175,12 +172,21 @@ async function getMejorCargador(ruta, autonomia, cargaBateria) {
         };
     }
 
+    let ultimaBusqueda = null;
     let cargadores = [];
     for (; puntoParaBuscar >= 0; puntoParaBuscar--) {
+        const distUltimaBusqueda = ultimaBusqueda !== null ?
+            google.maps.geometry.spherical.computeDistanceBetween(ruta[puntoParaBuscar], ultimaBusqueda) / 1000 :
+            Infinity;
+        if (puntoParaBuscar > 0 && distUltimaBusqueda < radioBusqueda) {
+            continue;
+        }
+
+        ultimaBusqueda = ruta[puntoParaBuscar];
         cargadores = await getCargadores({
-            distancia: 25, 
-            lat: ruta[puntoParaBuscar].lat(),
-            lng: ruta[puntoParaBuscar].lng()
+            distancia: radioBusqueda, 
+            lat: ultimaBusqueda.lat(),
+            lng: ultimaBusqueda.lng()
         });
         if (cargadores.length !== 0) break;
     }
@@ -194,8 +200,9 @@ async function getMejorCargador(ruta, autonomia, cargaBateria) {
         return a.AddressInfo.Distance - b.AddressInfo.Distance;
     })[0];
 
-    const marcador = markers.get(mejorCargador.ID);
+    const marcador = addMarcadorCargador(mejorCargador);
     marcador.setIcon("media/electricidad_seleccionada.png");
+    marcador.setVisible(true);
 
     return {
         rutaRestante: ruta.slice(puntoParaBuscar + 1),
@@ -211,7 +218,7 @@ const markers = new Map();
 
 async function getCargadores(coordenadas = null) {
     const url = "https://api.openchargemap.io/v3/poi";
-    let query = "statustypeid=50&usagetypeid=1,4,5,7&maxresults=1000&countrycode=ES&camelcase=false&key=ee289fb2-57ea-453d-84ce-70d9906b84d9";
+    let query = "statustypeid=50&usagetypeid=1,4,5,7&minpowerkw=100&maxresults=1000&countrycode=ES&camelcase=false&key=ee289fb2-57ea-453d-84ce-70d9906b84d9";
     if (coordenadas) {
         const {distancia, lat, lng} = coordenadas;
         query += `&latitude=${lat}&longitude=${lng}&distance=${distancia}&distanceunit=2`;
@@ -225,39 +232,54 @@ async function getCargadores(coordenadas = null) {
     data.forEach(element => {
         const price = getPrice(element.UsageCost);
         element.PrecioCalculado = parseFloat(price.replace(',', '.'));
-
-        if (!markers.has(element.ID)) {
-            const marker = new google.maps.Marker({
-                position: { lat: element.AddressInfo.Latitude, lng: element.AddressInfo.Longitude },
-                map: map,
-                icon: "media/electricidad.png",
-            });
-
-            ((marker, element) => {
-                marker.addListener('click', () => {
-
-                    new google.maps.InfoWindow({
-                        content: '<h3>' + element.AddressInfo.Title + '</h3>' +
-                            '<p>Dirección: ' + element.AddressInfo.AddressLine1 + '</p>' +
-                            '<p>Precio: ' + price + ' €/kWh</p>' +
-                            '<p>Conexiones: <ul>' + element.Connections.map(conn => {
-                                return '<li>' + conn.CurrentType?.Title + ' - ' + conn.PowerKW + 'kW</li>'
-                            }) + '</ul></p>'
-                    }).open(map, marker);
-                });
-            })(marker, element);
-
-            markers.set(element.ID, marker);
-        }
     });
 
     return data;
 }
 
-function limpiarRuta() {
+function addMarcadorCargador(cargador) {
+    if (markers.has(cargador.ID)) return markers.get(cargador.ID);
+
+    const marker = new google.maps.Marker({
+        position: { lat: cargador.AddressInfo.Latitude, lng: cargador.AddressInfo.Longitude },
+        map: map,
+        icon: "media/electricidad.png",
+    });
+
+    ((marker, cargador) => {
+        marker.addListener('click', () => {
+
+            new google.maps.InfoWindow({
+                content: '<h3>' + cargador.AddressInfo.Title + '</h3>' +
+                    '<p>Dirección: ' + cargador.AddressInfo.AddressLine1 + '</p>' +
+                    '<p>Precio: ' + cargador.PrecioCalculado + ' €/kWh</p>' +
+                    '<p>Conexiones: <ul>' + cargador.Connections.map(conn => {
+                        return '<li>' + conn.CurrentType?.Title + ' - ' + conn.PowerKW + 'kW</li>'
+                    }) + '</ul></p>'
+            }).open(map, marker);
+        });
+    })(marker, cargador);
+
+    markers.set(cargador.ID, marker);
+    return marker;
+}
+
+function limpiarMarcadores() {
     for (const marker of markers.values()) {
         if (marker.getIcon() !== "media/electricidad.png") {
             marker.setIcon("media/electricidad.png");
         }
+        marker.setVisible(false);
     }
+}
+
+function setCenter(lat, lng) {
+    const center = new google.maps.LatLng(lat, lng);
+    map.setCenter(center);
+    myPosition.setPosition(center);
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({location: center}, (results, status) => {
+        if(status === google.maps.GeocoderStatus.OK && results[0])
+        document.getElementById("inputOrigen").value = results[0].formatted_address;
+    });
 }
